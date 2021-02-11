5d52be14a160d899acce73dbf82a0b8719ff10c2
Pixel Lifetime <maxymkarpinsky@gmail.com>
2021-02-11 16:12:32 +0200

    Update to have multi row arrays and support properties.

--- Assets/{@#} Answers/Creating a custom Editor Window using a Multi Column Header/MCHEditorWindow.cs
+++ Assets/{@#} Answers/Creating a custom Editor Window using a Multi Column Header/MCHEditorWindow.cs
@@ -12,17 +12,40 @@ public class MCHEditorWindow : EditorWindow
 	/// <summary>
 	/// This is a test object, just to showcase how this could be used.
 	/// </summary>
-	private class Enemy
+	[System.Serializable]
+	private class Enemy : ScriptableObject
 	{
-		public string Name { get; set; }
-		public float Health { get; set; }
-		public Color SkinColor { get; set; }
+		[SerializeField] private string _name;
+		public string Name => this._name;
+
+		[SerializeField] private float _health;
+		public float Health => this._health;
+
+		[SerializeField] private Color _skinColor;
+		public Color SkinColor => this._skinColor;
+
+		[SerializeField] private List<GameObject> _gameObjects;
+		public List<GameObject> GameObjects => this._gameObjects;
 
 		public Enemy(string name, float health, Color skinColor)
 		{
-			this.Name = name;
-			this.Health = health;
-			this.SkinColor = skinColor;
+			this._name = name;
+			this._health = health;
+			this._skinColor = skinColor;
+		}
+
+		public void Initialize(string name, float health, Color skinColor)
+		{
+			this._name = name;
+			this._health = health;
+			this._skinColor = skinColor;
+		}
+
+		private void Awake()
+		{
+			this._name = "Default Name";
+			this._health = 34.2f;
+			this._skinColor = Color.white;
 		}
 	}
 
@@ -47,21 +70,12 @@ public class MCHEditorWindow : EditorWindow
 	private MultiColumnHeaderState.Column[] _columns;
 
 	// Create a few test subjects.
-	private Enemy[] _testObjects = new Enemy[]
-	{
-		new Enemy("Orc", 25.0f, Color.green),
-		new Enemy("Fairy", 10.0f, Color.cyan),
-		new Enemy("Mech Golem", 57.0f, Color.grey),
-		new Enemy("Orc", 25.0f, Color.green),
-		new Enemy("Fairy", 10.0f, Color.cyan),
-		new Enemy("Mech Golem", 57.0f, Color.grey),
-		new Enemy("Orc", 25.0f, Color.green),
-		new Enemy("Fairy", 10.0f, Color.cyan),
-		new Enemy("Mech Golem", 57.0f, Color.grey),
-		new Enemy("Orc", 25.0f, Color.green),
-		new Enemy("Fairy", 10.0f, Color.cyan),
-		new Enemy("Mech Golem", 57.0f, Color.grey),
-	};
+	private Enemy[] _testObjects = new Enemy[3];
+	//{
+	//	new Enemy("Orc", 25.0f, Color.green),
+	//	new Enemy("Fairy", 10.0f, Color.cyan),
+	//	new Enemy("Mech Golem", 57.0f, Color.grey),
+	//};
 
 	private float _multiColumnHeaderWidth;
 
@@ -108,6 +122,16 @@ public class MCHEditorWindow : EditorWindow
 				headerContent = new GUIContent("Skin Color", "A color of an enemy skin."),
 				headerTextAlignment = TextAlignment.Center,
 			},
+			new MultiColumnHeaderState.Column()
+			{
+				allowToggleVisibility = false,
+				autoResize = true,
+				minWidth = 300.0f,
+				canSort = false,
+				sortingArrowAlignment = TextAlignment.Right,
+				headerContent = new GUIContent("Game Objects", "An array of game objects."),
+				headerTextAlignment = TextAlignment.Center,
+			},
 		};
 
 		this._multiColumnHeaderState = new MultiColumnHeaderState(columns: this._columns);
@@ -122,10 +146,20 @@ public class MCHEditorWindow : EditorWindow
 
 		// Initial resizing of the content.
 		this._multiColumnHeader.ResizeToFit();
+
+
+		this._testObjects[0] = ScriptableObject.CreateInstance<Enemy>();
+		this._testObjects[0].Initialize("Orc", 25.0f, Color.green);
+
+		this._testObjects[1] = ScriptableObject.CreateInstance<Enemy>();
+		this._testObjects[1].Initialize("Fairy", 10.0f, Color.cyan);
+
+		this._testObjects[2] = ScriptableObject.CreateInstance<Enemy>();
+		//this._testObjects[2].Initialize("Mech Golem", 57.0f, Color.grey);
 	}
 
-	private readonly Color _lighterColor = Color.white * 0.3f;
-	private readonly Color _darkerColor = Color.white * 0.1f;
+	private readonly Color _lighterColor = new Color(r: 1.0f, g: 1.0f, b: 1.0f, a: 0.15f);
+	private readonly Color _darkerColor = new Color(r: 0.0f, g: 0.0f, b: 0.0f, a: 0.15f);
 
 	private Vector2 _scrollPosition;
 
@@ -208,10 +242,16 @@ public class MCHEditorWindow : EditorWindow
 				// Draw header for columns here.
 				this._multiColumnHeader.OnGUI(rect: columnRectPrototype, xScroll: 0.0f);
 
+				float heightJump = columnHeight;
+
 				// For each element that we have in object that we are modifying.
 				//? I don't have an appropriate object here to modify, but this is just an example. In real world case I would probably use ScriptableObject here.
 				for (int a = 0; a < this._testObjects.Length; a++)
 				{
+					SerializedObject serializedObject = new SerializedObject(obj: this._testObjects[a]);
+
+					float calculatedRowHeight = 0.0f;
+
 					//! We draw each type of field here separately because each column could require a different type of field as seen here.
 					// This can be improved if we want to have a more robust system. Like for example, we could have logic of drawing each field moved to object itself.
 					// Then here we would be able to just iterate through array of these objects and call a draw methods for these fields and use this window for many types of objects.
@@ -219,13 +259,7 @@ public class MCHEditorWindow : EditorWindow
 
 					Rect rowRect = new Rect(source: columnRectPrototype);
 
-					rowRect.y += columnHeight * (a + 1);
-
-					// Draw a texture before drawing each of the fields for the whole row.
-					if (a % 2 == 0)
-						EditorGUI.DrawRect(rect: rowRect, color: this._darkerColor);
-					else
-						EditorGUI.DrawRect(rect: rowRect, color: this._lighterColor);
+					//rowRect.y += columnHeight * (a + 1);
 
 					// Name field.
 					int columnIndex = 0;
@@ -238,7 +272,8 @@ public class MCHEditorWindow : EditorWindow
 
 						// This here basically is a row height, you can make it any value you like. Or you could calculate the max field height here that your object has and store it somewhere then use it here instead of `EditorGUIUtility.singleLineHeight`.
 						// We move position of field on `y` by this height to get correct position.
-						columnRect.y = rowRect.y;
+						columnRect.y = rowRect.y + heightJump;
+						//columnRect.height += heightJump;
 
 						GUIStyle nameFieldGUIStyle = new GUIStyle(GUI.skin.label)
 						{
@@ -261,13 +296,19 @@ public class MCHEditorWindow : EditorWindow
 
 						Rect columnRect = this._multiColumnHeader.GetColumnRect(visibleColumnIndex: visibleColumnIndex);
 
-						columnRect.y = rowRect.y;
+						columnRect.y = rowRect.y + heightJump;
+						//columnRect.height += heightJump;
 
-						this._testObjects[a].Health = EditorGUI.Slider(
+						SerializedProperty serializedProperty = serializedObject.FindProperty(propertyPath: "_health");
+
+						calculatedRowHeight = Mathf.Max(calculatedRowHeight, EditorGUI.GetPropertyHeight(property: serializedProperty, includeChildren: true));
+
+						EditorGUI.Slider(
 							position: this._multiColumnHeader.GetCellRect(visibleColumnIndex: visibleColumnIndex, columnRect),
-							value: this._testObjects[a].Health,
+							property: serializedProperty,
 							leftValue: 0.0f,
-							rightValue: 100.0f
+							rightValue: 100.0f,
+							label: GUIContent.none
 						);
 					}
 
@@ -280,17 +321,58 @@ public class MCHEditorWindow : EditorWindow
 
 						Rect columnRect = this._multiColumnHeader.GetColumnRect(visibleColumnIndex: visibleColumnIndex);
 
-						columnRect.y = rowRect.y;
+						columnRect.y = rowRect.y + heightJump;
+						//columnRect.height += heightJump;
 
-						this._testObjects[a].SkinColor = EditorGUI.ColorField(
+						SerializedProperty serializedProperty = serializedObject.FindProperty(propertyPath: "_skinColor");
+
+						calculatedRowHeight = Mathf.Max(calculatedRowHeight, EditorGUI.GetPropertyHeight(property: serializedProperty, includeChildren: true));
+
+						EditorGUI.PropertyField(
 							position: this._multiColumnHeader.GetCellRect(visibleColumnIndex: visibleColumnIndex, columnRect),
-							label: GUIContent.none,
-							value: this._testObjects[a].SkinColor,
-							showEyedropper: true,
-							showAlpha: false,
-							hdr: false
+							property: serializedProperty,
+							label: GUIContent.none
 						);
 					}
+
+					// Skin color field.
+					columnIndex = 3;
+
+					if (this._multiColumnHeader.IsColumnVisible(columnIndex: columnIndex))
+					{
+						int visibleColumnIndex = this._multiColumnHeader.GetVisibleColumnIndex(columnIndex: columnIndex);
+
+						Rect columnRect = this._multiColumnHeader.GetColumnRect(visibleColumnIndex: visibleColumnIndex);
+
+						columnRect.y = rowRect.y + heightJump;
+						//columnRect.height += heightJump;
+
+						SerializedProperty serializedProperty = serializedObject.FindProperty(propertyPath: "_gameObjects");
+
+						calculatedRowHeight = Mathf.Max(calculatedRowHeight, EditorGUI.GetPropertyHeight(property: serializedProperty, includeChildren: true));
+
+						EditorGUI.PropertyField(
+							position: this._multiColumnHeader.GetCellRect(visibleColumnIndex: visibleColumnIndex, columnRect),
+							property: serializedProperty,
+							includeChildren: true
+						);
+					}
+
+					// The problem of it drawing here is that it draws over fields instead of under them.
+					//? The soultion could be to cache height for each row after 1 OnGUI and use it instead, upading it each time it reaches the end.
+					Rect backgroundColorRect = new Rect(source: rowRect)
+					{
+						y = rowRect.y + heightJump,
+						height = calculatedRowHeight
+					};
+
+					// Draw a texture before drawing each of the fields for the whole row.
+					if (a % 2 == 0)
+						EditorGUI.DrawRect(rect: backgroundColorRect, color: this._darkerColor);
+					else
+						EditorGUI.DrawRect(rect: backgroundColorRect, color: this._lighterColor);
+
+					heightJump += calculatedRowHeight;
 				}
 			}
 			GUI.EndScrollView(handleScrollWheel: true);
